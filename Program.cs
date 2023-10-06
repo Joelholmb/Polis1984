104d30d2fa158e95c8ded21380937531c02bfec6
Joel Holmberg <joel.holmberg@student.nbi-handelsakademin.se>
2023-10-06 11:56:28 +0200

    flyttat ut valjPolis ur main

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Text.Json;
 
 
+
 namespace Polis_1984;
 
     public class Polis
@@ -40,7 +41,41 @@ namespace Polis_1984;
 
     class Program
     {
-            public static PolisLista polisLista = new PolisLista();
+        public static PolisLista polisLista = new PolisLista();
+
+        public static Polis ValjPolis(List<Polis> valdaPoliser)
+        {
+            Console.WriteLine("Välj en polis från listan:");
+
+            List<Polis> listaAvPoliser = polisLista.HämtaPoliser();
+            for (int i = 0; i < listaAvPoliser.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {listaAvPoliser[i].Namn} (Tjänstenummer: {listaAvPoliser[i].Tjanstenummer}){Environment.NewLine}");
+            }
+
+            int val;
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out val) && val >= 1 && val <= listaAvPoliser.Count)
+                {
+                    Polis valdPolis = listaAvPoliser[val - 1];
+                    if (!valdaPoliser.Contains(valdPolis))
+                    {
+                        valdaPoliser.Add(valdPolis);
+                        listaAvPoliser.RemoveAt(val - 1);
+                        return valdPolis;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Denna polis har redan valts. Välj en annan.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Personen finns inte i listan. Försök igen.");
+                }
+            }
+        }
 
         static void Main(string[] args)
         {
@@ -87,48 +122,8 @@ namespace Polis_1984;
                     Console.WriteLine("Ogiltigt val");
                 }
             }
-            
-        public static Polis ValjPolis(List<Polis> valdaPoliser)
-        {
-            Console.WriteLine("Välj en polis från listan:");
-            
-            List<Polis> listaAvPoliser = polisLista.HämtaPoliser();
-            for (int i = 0; i < listaAvPoliser.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}. {listaAvPoliser[i].Namn} (Tjänstenummer: {listaAvPoliser[i].Tjanstenummer}){Environment.NewLine}");
-            }
-
-            
-
-                int val;
-                while (true)
-                
-
-                {
-                    if (int.TryParse(Console.ReadLine(), out val) && val >= 1 && val <= listaAvPoliser.Count)
-                    {
-                        Polis valdPolis = listaAvPoliser[val - 1];
-                        if (!valdaPoliser.Contains(valdPolis))
-                        {
-                            valdaPoliser.Add(valdPolis);
-                            listaAvPoliser.RemoveAt(val - 1);
-                            return valdPolis;
-                        
-                        }
-                        else
-                        {
-                            Console.WriteLine("Denna polis har redan valts. Välj en annan.");
-                        }
-                    }    
-                    else
-                    {
-                    Console.WriteLine("Personen finns inte i listan. Försök igen.");
-                    }
-                }
-                
-        }
     
-    }
+        }
     }
 
 class Utryckning
@@ -231,17 +226,4 @@ class Rapport
         }
     }
 
-}
-
-class Polis
-{
-    public string namn;
-
-    public int tjanstenummer;
-
-    public Polis(string namn, int tjanstenummer)
-    {
-        this.namn = namn;
-        this.tjanstenummer = tjanstenummer;
-    }
 }
