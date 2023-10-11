d980571ef04ce62833c95d0007a30531c0f11ecc
Joel Holmberg <joel.holmberg@student.nbi-handelsakademin.se>
2023-10-11 13:44:02 +0200

    separat polisklass

---
+++ Polis.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Polis_1984
+{
+    public class Polis
+    {
+        public string Namn { get; set;}
+        public int Tjanstenummer { get; set;}
+
+        public Polis(string namn, int tjanstenummer)
+        {
+            Namn = namn;
+            Tjanstenummer = tjanstenummer;
+        }
+    }
+}
